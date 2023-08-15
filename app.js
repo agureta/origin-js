6421e1085049cd13ad00498f1745cc73a533971d
Agustin Retamar <agustinretamar97@gmail.com>
2023-08-15 20:55:13 -0300

    segunda preentrega app funcionando

--- app.js
+++ app.js
@@ -1,26 +1,102 @@
-let resultado = 0
-let repemax = "tu 1RM es: \n"
+class Rutina {
+    constructor(id, ejercicio, series){
+        this.id = id;
+        this.ejercicio = ejercicio;
+        this.series = series;
+    }
 
-let rta= ""
+    aumentarSeries(series){
+        this.series = this.series + series;
+    }
 
-do{
-let repeticiones=Number (prompt("cuantas repeticiones realizaste"))
-let peso=Number (prompt("con cuanto peso x cada repe?"))
-
-rta = prompt ("desea ver el resultado? escribe 'si'")
-
-resultado = resultado + (peso*(1+(0.025*repeticiones)))
-repemax = repemax + resultado + "kg"
-
-} while (rta != "si")
-
-alert(repemax)
-
-
-if(resultado > 100){
-    alert("estas muy fuerte")
-} else{
-    alert("segui entrenando!")
+    decripcion(){
+        return "Ejercicio: " + this.ejercicio +
+                "Series: " + this.series;
+    }
 }
 
+class ControladorTrabajo{
+    constructor(){
+        this.listaEjercicios = [];
+    }
+
+
+    agregar(ejercicio){
+        this.listaEjercicios.push(ejercicio);
+    }
+
+    cantidadDeSeries(series){
+        this.listaEjercicios.push(series)
+    }
+
+
+    buscarEjercicioPorID(id){
+        return this.listaEjercicios.find(ejercicio => ejercicio.id == id);
+    }
+
+    mostrarEjercicios(){
+        let listaEnTexto = "";
+        this.listaEjercicios.forEach(ejercicio => {
+            listaEnTexto = listaEnTexto  + " ";
+        });
+        alert(listaEnTexto);
+    }
+}
+
+class ejerciciosRealizados{
+    constructor(){
+        this.listaCarrito = [];
+    }
+
+    agregar(ejercicio, series){
+        let existe = this.listaCarrito.some(el => el.id == ejercicio.id);
+        if(existe){
+            ejercicio.aumentarSeries(series);
+        } else {
+            ejercicio.aumentarSeries(series);
+            this.listaCarrito.push(ejercicio);
+        }
+    }
+
+    mostrarEjercicios(){
+        let listaEnTexto = "Opciones de ejercicios: ";
+        this.listaCarrito.forEach(ejercicio => {
+            listaEnTexto = listaEnTexto + " " + ejercicio.decripcion() + "\n";
+        });
+        alert(listaEnTexto);
+    }
+
+    calcularTotalDeSeries(){
+        return this.listaCarrito.reduce((acumulador, ejercicio) => acumulador +  ejercicio.ejercicio + ejercicio.series, 0);
+    }
+}
+
+const CT = new ControladorTrabajo();
+const CARRITO = new ejerciciosRealizados();
+
+CT.agregar(new Rutina(1, "press banca"));
+CT.agregar(new Rutina(2, "sentadilla"));
+CT.agregar(new Rutina(3, "dominadas"));
+CT.agregar(new Rutina(4, "Curl de biceps"));
+CT.agregar(new Rutina(5, "Extension de cuadriceps"));
+
+let rta;
+let resumenEjercicios = "";
+
+do {
+    CT.mostrarEjercicios();
+    let opcion = Number(prompt("Ingrese el código del ejercicio que desea agregar"));
+    let ejercicio = CT.buscarEjercicioPorID(opcion);
+    let series = Number(prompt("Ingrese la cantidad de series que realizó"));
+
+    CARRITO.agregar(ejercicio, series);
+    CARRITO.cantidadDeSeries(ejercicio, series)
+    resumenEjercicios += CARRITO.mostrarEjercicios();
+
+    rta = prompt("Ingrese ESC para salir").toUpperCase();
+} while (rta !== "ESC");
+
+resumenEjercicios += "Hoy realizó: " + calcularTotalDeSeries();
+
+alert(resumenEjercicios);
 
