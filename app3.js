e9b07d89ffda939000de1a218257688ac5115fcb
Agustin Retamar <agustinretamar97@gmail.com>
2023-08-15 22:19:06 -0300

    segunda preentrega funcionando 10.20

--- app3.js
+++ app3.js
@@ -2,16 +2,16 @@ class Rutina {
     constructor(id, ejercicio, series){
         this.id = id;
         this.ejercicio = ejercicio;
-        this.series = series;
+        this.series = 0;
     }
 
     aumentarSeries(series){
         this.series = this.series + series;
     }
 
-    decripcion(){
-        return "Ejercicio: " + this.ejercicio +
-                "Series: " + this.series;
+    descripcion(){
+        return "ejercicio " + this.id + ": " + this.ejercicio + " " +
+               "series: " + this.series + "\n";
     }
 }
 
@@ -20,15 +20,9 @@ class ControladorTrabajo{
         this.listaEjercicios = [];
     }
 
-
     agregar(ejercicio){
         this.listaEjercicios.push(ejercicio);
     }
-
-    cantidadDeSeries(series){
-        this.listaEjercicios.push(series)
-    }
-
 
     buscarEjercicioPorID(id){
         return this.listaEjercicios.find(ejercicio => ejercicio.id == id);
@@ -37,13 +31,13 @@ class ControladorTrabajo{
     mostrarEjercicios(){
         let listaEnTexto = "";
         this.listaEjercicios.forEach(ejercicio => {
-            listaEnTexto = listaEnTexto  + " ";
+            listaEnTexto = listaEnTexto + ejercicio.descripcion() + " ";
         });
         alert(listaEnTexto);
     }
 }
 
-class ejerciciosRealizados{
+class CarritoDeEjercicios{
     constructor(){
         this.listaCarrito = [];
     }
@@ -59,11 +53,15 @@ class ejerciciosRealizados{
     }
 
     mostrarEjercicios(){
-        let listaEnTexto = "Opciones de ejercicios: ";
+        let listaEnTexto = "Carrito de ejercicios: ";
         this.listaCarrito.forEach(ejercicio => {
-            listaEnTexto = listaEnTexto + " " + ejercicio.decripcion() + "\n";
+            listaEnTexto = listaEnTexto + ejercicio.descripcion();
         });
         alert(listaEnTexto);
+    }
+
+    calcularTotal(){
+        return this.listaCarrito.reduce((acumulador, ejercicio) => acumulador + ejercicio.series, 0);
     }
 
     calcularTotalDeSeries(){
@@ -72,31 +70,28 @@ class ejerciciosRealizados{
 }
 
 const CT = new ControladorTrabajo();
-const CARRITO = new ejerciciosRealizados();
+const CARRITO = new CarritoDeEjercicios();
 
-CT.agregar(new Rutina(1, "press banca"));
-CT.agregar(new Rutina(2, "sentadilla"));
-CT.agregar(new Rutina(3, "dominadas"));
-CT.agregar(new Rutina(4, "Curl de biceps"));
-CT.agregar(new Rutina(5, "Extension de cuadriceps"));
+CT.agregar(new Rutina(1,"Press banca"));
+CT.agregar(new Rutina(2,"Sentadilla"));
+CT.agregar(new Rutina(3,"Dominadas"));
+CT.agregar(new Rutina(4,"Curl Biceps"));
+CT.agregar(new Rutina(5,"Extension de cuadriceps"));
+CT.agregar(new Rutina(6,"Press militar"));
 
 let rta;
-let resumenEjercicios = "";
 
-do {
+do{
     CT.mostrarEjercicios();
-    let opcion = Number(prompt("Ingrese el código del ejercicio que desea agregar"));
+    let opcion = Number(prompt("Ingrese el codigo del producto que desea agregar"));
     let ejercicio = CT.buscarEjercicioPorID(opcion);
-    let series = Number(prompt("Ingrese la cantidad de series que realizó"));
+    let series = Number(prompt("Ingrese la cantidad de series que realizo"));
 
     CARRITO.agregar(ejercicio, series);
-    CARRITO.cantidadDeSeries(ejercicio, series)
-    resumenEjercicios += CARRITO.mostrarEjercicios();
+    alert("El ejercicio se agrego");
+    CARRITO.mostrarEjercicios();
 
     rta = prompt("Ingrese ESC para salir").toUpperCase();
-} while (rta !== "ESC");
+} while(rta != "ESC");
 
-resumenEjercicios += "Hoy realizó: " + calcularTotalDeSeries();
-
-alert(resumenEjercicios);
-
+alert("Hoy realizo: " + CARRITO.calcularTotal() + " series.");
